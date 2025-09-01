66f5980af00897f5268167c9119eb2bb5826987a
florenz00110001-00110001 <florenzjedquiod@gmail.com>
2025-09-01 13:18:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Cache version
+const CACHE_NAME = "guitar-trainer-v1";
+
+// Files to cache
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// Install service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate service worker (clean old caches)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+// Fetch assets
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
